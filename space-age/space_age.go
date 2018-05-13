d244b1fc47e19adf1ba14a4956b06e257ae2dae5
riacataquian <icataquian@gmail.com>
2018-05-14 01:47:59 +0800

    Make space age's tests pass

--- space-age/space_age.go
+++ space-age/space_age.go
@@ -1,29 +1,33 @@
-// package space ...
+// Package space contains helpers and values for space age computations.
 package space
 
-// Planet ...
+// Planet describes a planet object.
 type Planet string
 
-// Age ...
-func Age(s float64, p Planet) (age float64) {
-	switch p {
-	case "Mercury":
-		age = 280.88
-	case "Venus":
-		age = 9.78
-	case "Mars":
-		age = 39.25
-	case "Jupiter":
-		age = 2.41
-	case "Saturn":
-		age = 3.23
-	case "Uranus":
-		age = 1.21
-	case "Neptune":
-		age = 1.58
-	default:
-		age = 31.69
+// PlanetYears describes the mapping of planets and their earth years.
+type PlanetYears map[Planet]float64
+
+// lookup is the lookup table for planet years.
+var lookup PlanetYears
+
+// Register all planets.
+func init() {
+	lookup = make(PlanetYears)
+	lookup["Earth"] = 31557600
+	lookup["Mercury"] = 0.2408467
+	lookup["Venus"] = 0.61519726
+	lookup["Mars"] = 1.8808158
+	lookup["Jupiter"] = 11.862615
+	lookup["Saturn"] = 29.447498
+	lookup["Uranus"] = 84.016846
+	lookup["Neptune"] = 164.79132
+}
+
+// Age computes for the Earth-years old given seconds olds.
+func Age(s float64, p Planet) float64 {
+	if p == "Earth" {
+		return s / lookup["Earth"]
 	}
 
-	return
+	return (s / lookup[p]) / lookup["Earth"]
 }
