773e62e7506c25263a9bf5a97d4e493bcc59cd8c
riacataquian <icataquian@gmail.com>
2018-05-14 20:36:01 +0800

    Makes hamming tests pass

---
+++ hamming/hamming.go
@@ -0,0 +1,29 @@
+// package hamming measures the minimum number of point mutations
+// that could have occurred on the evolutionary path between two strands.
+package hamming
+
+import (
+	"errors"
+)
+
+// Distance calcualtes difference between a and b stands.
+func Distance(a, b string) (int, error) {
+	// Prevent unmatched strand lenghts
+	if len(a) != len(b) {
+		return -1, errors.New("Mismatched lenghts")
+	}
+
+	// If strands matched, then return 0 diff.
+	if a == b {
+		return 0, nil
+	}
+
+	// Compare per letter, accumulate diff if unmatched string value.
+	var diff int
+	for i, l := range a {
+		if string(l) != string(b[i]) {
+			diff++
+		}
+	}
+	return diff, nil
+}
