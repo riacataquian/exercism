16d6f12c3aab30fcfe0d38fe75ac414fd7511b85
riacataquian <icataquian@gmail.com>
2018-06-13 20:02:08 +0800

    Makes linter happy

--- parallel-letter-frequency/frequency.go
+++ parallel-letter-frequency/frequency.go
@@ -1,11 +1,19 @@
+// Package letter ...
 package letter
 
+// FreqMap ...
 type FreqMap map[rune]int
 
+// Frequency ...
 func Frequency(s string) FreqMap {
 	m := FreqMap{}
 	for _, r := range s {
 		m[r]++
 	}
 	return m
+}
+
+// ConcurrentFrequency ...
+func ConcurrentFrequency(strs []string) FreqMap {
+	return FreqMap{}
 }
