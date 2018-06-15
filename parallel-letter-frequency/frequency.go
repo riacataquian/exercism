76948ec7dd4c62157bd72059dbc2891643d4f3c6
riacataquian <icataquian@gmail.com>
2018-06-15 20:04:30 +0800

    Resolves tests

--- parallel-letter-frequency/frequency.go
+++ parallel-letter-frequency/frequency.go
@@ -1,8 +1,18 @@
-// Package letter counts the frequency of letters in texts using parallel computation.
+// Package letter counts the frequency of letters in strs using parallel computation.
 package letter
+
+import (
+	"sync"
+)
 
 // FreqMap maps runes and their frequency counts.
 type FreqMap map[rune]int
+
+// SafeCounter holds the frequency map and its locker.
+type SafeCounter struct {
+	v   FreqMap
+	mux sync.Mutex
+}
 
 // Frequency counts the runes and their frequency synchronously.
 func Frequency(s string) FreqMap {
@@ -15,5 +25,20 @@ func Frequency(s string) FreqMap {
 
 // ConcurrentFrequency counts the runes and their frequency concurrently.
 func ConcurrentFrequency(strs []string) FreqMap {
-	return FreqMap{}
+	c := SafeCounter{v: make(FreqMap)}
+
+	for _, str := range strs {
+		for _, s := range str {
+			go c.Inc(s)
+		}
+	}
+
+	return c.v
+}
+
+// Inc increments a key's count.
+func (c *SafeCounter) Inc(r rune) {
+	c.mux.Lock()
+	c.v[r]++ // Lock so only one goroutine at a time can access the map c.v.
+	c.mux.Unlock()
 }
