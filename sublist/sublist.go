43c6531afe53d31014d430f862ae4c5fa69378db
riacataquian <icataquian@gmail.com>
2018-06-30 17:24:12 +0800

    Resolves tests until - false start

--- sublist/sublist.go
+++ sublist/sublist.go
@@ -1,1 +1,66 @@
+// Package sublist ...
 package sublist
+
+import (
+// "log"
+)
+
+// Relation ...
+type Relation string
+
+const (
+	Unequal Relation = "unequal"
+	Equal   Relation = "equal"
+	Sub     Relation = "sublist"
+	Sup     Relation = "superlist"
+	Wat     Relation = "wat"
+)
+
+func Sublist(a, b []int) Relation {
+	if len(a) == 0 && len(b) == 0 {
+		return Equal
+	}
+
+	if sub(a, b) {
+		return Sub
+	}
+
+	if sup(b, a) {
+		return Sup
+	}
+
+	if equal(a, b) {
+		return Equal
+	}
+
+	return Unequal
+}
+
+func equal(a, b []int) bool {
+	var matching int
+	for _, m := range a {
+		for _, n := range b {
+			if m == n {
+				matching++
+			}
+		}
+	}
+
+	return matching == len(a)
+}
+
+func sub(a, b []int) bool {
+	if len(a) == 0 {
+		return true
+	}
+
+	return false
+}
+
+func sup(a, b []int) bool {
+	if len(a) == 0 {
+		return true
+	}
+
+	return false
+}
